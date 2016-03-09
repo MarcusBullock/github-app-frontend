83cf2405216085fe6b8112adcd53f11a6bdba8fa
Vicky Gray <vickygray26@hotmail.com>
2016-03-09 18:57:05 +0000

    fixed access to angular

--- test/e2e/listSpec.js
+++ test/e2e/listSpec.js
@@ -1,9 +1,9 @@
 describe("Github application", function(){
 
   it("Displays a list", function(){
-    browser.get("http://localhost:8000");
+    browser.get("http://localhost:8080");
     var user = element.all(by.css(".user-class")).first();
-    expect(user.getText()).toContain("Name: vicky Repo count: 10 Follower count: 32");
+    expect(user.getText()).toContain("Username: vicky Repo count: 10 Follower count: 32");
   });
 
 });
