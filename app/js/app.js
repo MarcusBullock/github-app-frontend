b99a6acf9d6cc826cc32ad3f9296ad015d0c672b
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-10 16:53:04 +0000

    angular messaround;

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,8 @@
 var githubApp = angular.module('githubApp', ['ngResource']);
+  app.controller('MyCtrl', function($scope, $http) {
+    $scope.loadData = function () {
+    $http.get('/Users/ScottGledhill/Desktop/Projects/GithubApp').success(function(data) {
+      $scope.items = data;
+    });
+  };
+});
