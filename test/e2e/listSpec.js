fdc0d4de360bc7cde5f0c5d2a760e8927370c121
Vicky Gray <vickygray26@hotmail.com>
2016-03-10 14:37:12 +0000

    added search bar

--- test/e2e/listSpec.js
+++ test/e2e/listSpec.js
@@ -6,4 +6,15 @@ describe("Github application", function(){
     expect(user.getText()).toContain("Username: vicky\nRepo count: 10\nFollower count: 32");
   });
 
+  it('Filters users from the list based on name/alphabetical order', function() {
+   var userList = element.all(by.repeater(user in listCtrl.users));
+   var query = element(by.model('query'));
+
+   expect(userList.count()).toBe(3);
+   query.sendKeys('Scott');
+   expect(userList.count()).toBe(1);
+   query.clear();
+   query.sendKeys('Marcus');
+   expect(userList.count()).toBe(whatevs);
+ });
 });
