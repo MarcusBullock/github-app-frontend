4b6cb59273460452f85feadc36f9c36b4fb173df
Vicky Gray <vickygray26@hotmail.com>
2016-03-10 11:53:43 +0000

    fixed test

--- test/e2e/listSpec.js
+++ test/e2e/listSpec.js
@@ -3,7 +3,7 @@ describe("Github application", function(){
   it("Displays a list", function(){
     browser.get("http://localhost:8080");
     var user = element.all(by.css(".user-class")).first();
-    expect(user.getText()).toContain("Username: vicky Repo count: 10 Follower count: 32");
+    expect(user.getText()).toContain("Username: vicky\nRepo count: 10\nFollower count: 32");
   });
 
 });
